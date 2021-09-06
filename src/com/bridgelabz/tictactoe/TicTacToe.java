6b3262be724549144e3e94a780a087594d7dd240
namancg <naman.c@perfios.com>
2021-09-06 08:52:31 +0530

    ADDED: method to get players choice

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -1,17 +1,33 @@
 package com.bridgelabz.tictactoe;
-
+import java.util.*;
 public class TicTacToe {
 	static char[] board= new char[10];
+	static char playerPawn,computer;
+	static Scanner sc= new Scanner(System.in);
 		public static void main(String args[])
 		{
 			System.out.println("TICTACTOE GAME");
 			createBoard();
-			
+			choosePawn();
 		}
 		static void createBoard() {
-			for( int i=0;i<10;i++)
+			for( int i=1;i<10;i++)
 			{
 					board[i]=' ';
 			}
+		}
+		static void choosePawn() {
+			System.out.println("CHOOSE THE PAWN TO PLAY WITH");
+			playerPawn = Character.toUpperCase(sc.next().charAt(0));
+			if(playerPawn == 'X')
+			{
+				System.out.println("YOU HAVE CHOSEN X");
+				computer ='O';
+				
+			}
+			else
+				computer ='X';
+			
+			
 		}
 }
